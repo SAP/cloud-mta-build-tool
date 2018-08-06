e695a22a6614769bbe43b884f46b5a3311430741
tals <shimon.tal@sap.com>
2018-08-06 16:52:37 +0300

    Constants update code style

--- cmd/mta/metainfo/desc.go
+++ cmd/mta/metainfo/desc.go
@@ -23,48 +23,48 @@ import (
 // The name sections with the MTA module attribute indicates the path to the file or directory which represents a module within the archive
 
 const (
-	META_INF      = "/META-INF"
-	MANIFEST      = "MANIFEST.MF"
-	MTAD          = "mtad.yaml"
-	NEW_LINE      = "\n"
-	CONT_TYPE_APP = "Content-Type: application/zip"
-	MTA_PRE       = "MTA-Module: "
-	MOD_NAME      = "Name: "
-	MANIFEST_VER  = "Manifest-Version: 1.0 \n"
+	MetaInf         = "/META-INF"
+	Manifest        = "MANIFEST.MF"
+	Mtad            = "mtad.yaml"
+	NewLine         = "\n"
+	ContentType     = "Content-Type: application/zip"
+	MtaModule       = "MTA-Module: "
+	ModuleName      = "Name: "
+	ManifestVersion = "Manifest-Version: 1.0 \n"
 )
 
 func setManifetDesc(file io.Writer, mtaStr []*models.Modules) {
 	// TODO create dynamically
-	fmt.Fprint(file, MANIFEST_VER)
+	fmt.Fprint(file, ManifestVersion)
 	// TODO set the version from external config for automatic version bump during release
 	fmt.Fprint(file, "Created-By: SAP Application Archive Builder 0.0.1")
 	for _, mod := range mtaStr {
 
-		fmt.Fprint(file, NEW_LINE)
-		fmt.Fprint(file, NEW_LINE)
-		fmt.Fprint(file, MOD_NAME+mod.Name+constants.DataZip)
-		fmt.Fprint(file, NEW_LINE)
-		fmt.Fprint(file, MTA_PRE+mod.Name)
-		fmt.Fprint(file, NEW_LINE)
-		fmt.Fprint(file, CONT_TYPE_APP)
+		fmt.Fprint(file, NewLine)
+		fmt.Fprint(file, NewLine)
+		fmt.Fprint(file, ModuleName+mod.Name+constants.DataZip)
+		fmt.Fprint(file, NewLine)
+		fmt.Fprint(file, MtaModule+mod.Name)
+		fmt.Fprint(file, NewLine)
+		fmt.Fprint(file, ContentType)
 
 	}
 }
 
 func GenMetaInf(tmpDir string, mtaStr models.MTA) {
 	// Create META-INF folder under the mtar folder
-	dir.CreateDirIfNotExist(tmpDir + META_INF)
+	dir.CreateDirIfNotExist(tmpDir + MetaInf)
 	// Modify MTAD object
 	converter.ModifyMtad(mtaStr)
 	// Create readable Yaml before writing to file
 	mtad := mta.Marshal(mtaStr)
 	// Write back the mtad to the META-INF folder
-	err := ioutil.WriteFile(tmpDir+META_INF+constants.PathSep+MTAD, mtad, os.ModePerm)
+	err := ioutil.WriteFile(tmpDir+MetaInf+constants.PathSep+Mtad, mtad, os.ModePerm)
 	if err != nil {
 		log.Println(err)
 	}
 	// Create MANIFEST.MF file
-	file := dir.CreateFile(tmpDir + META_INF + constants.PathSep + MANIFEST)
+	file := dir.CreateFile(tmpDir + MetaInf + constants.PathSep + Manifest)
 	// Set the MANIFEST.MF file
 	setManifetDesc(file, mtaStr.Modules)
 }
