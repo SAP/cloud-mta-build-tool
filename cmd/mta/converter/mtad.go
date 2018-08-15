a77eb998d9a08d678f2a753fe448256b2b79c1c0
tals <shimon.tal@sap.com>
2018-08-15 09:43:17 +0300

    [chore] - Cleanup

--- cmd/mta/converter/mtad.go
+++ cmd/mta/converter/mtad.go
@@ -9,23 +9,20 @@ import (
 
 // ConvertTypes file according to the deployed env
 func ConvertTypes(mta models.MTA, platforms platform.Platforms, platform string) {
+	//todo get from config
 	const (
 		SCHEMA_VERSION = "3.1"
 	)
-
 	for _, module := range platforms.Platforms {
 
 		if module.Name == platform {
-
 			// Modify schema version
 			*mta.SchemaVersion = SCHEMA_VERSION
 			// Modify Types
 			for i, value := range module.Models {
 				//Check for types
 				if len(mta.Modules) > i {
-
-					switch strings.Compare(value.NativeType, mta.Modules[i].Type) {
-					case 0:
+					if strings.Compare(value.NativeType, mta.Modules[i].Type)  == 0 {
 						//Modify the module type according the platform config
 						mta.Modules[i].Type = value.PlatformType
 					}
