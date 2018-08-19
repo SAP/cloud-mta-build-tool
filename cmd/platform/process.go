8395cdda30da588eb817d28ee74c1432464e579d
tals <shimon.tal@sap.com>
2018-08-19 11:42:23 +0300

    [feat] - cont. support builders commands configuration

--- cmd/platform/process.go
+++ cmd/platform/process.go
@@ -1,10 +1,9 @@
 package platform
 
 import (
-	"log"
-	"os"
-
 	"gopkg.in/yaml.v2"
+
+	"cloud-mta-build-tool/cmd/logs"
 )
 
 func Parse(data []byte) (Platforms) {
@@ -12,14 +11,7 @@ func Parse(data []byte) (Platforms) {
 	platforms := Platforms{}
 	err := yaml.Unmarshal(data, &platforms)
 	if err != nil {
-		log.Printf("Yaml file is not valid, Error: " + err.Error())
-		os.Exit(-1)
+		logs.Logger.Error("Yaml file is not valid, Error: " + err.Error())
 	}
 	return platforms
 }
-
-
-
-
-
-
