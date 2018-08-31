ad5aa3119ce75b1f3e9d6da89f2656f88429938c
Shapira <yotam.shapira@sap.com>
2018-08-31 23:30:13 +0300

    Add a simple integration test for commands provider

--- cmd/builders/commands_test.go
+++ cmd/builders/commands_test.go
@@ -3,9 +3,10 @@ package builders
 import (
 	"cloud-mta-build-tool/cmd/logs"
 	"cloud-mta-build-tool/cmd/mta/models"
-	"gopkg.in/yaml.v2"
 	"reflect"
 	"testing"
+
+	"gopkg.in/yaml.v2"
 )
 
 func TestExeCmd(t *testing.T) {
@@ -52,13 +53,13 @@ builders:
 			name: "Command for required module type",
 			args: args{
 				modules: models.Modules{
-					Name:        "uiapp",
-					Type:        "html5",
-					Path:        "./",
-					Requires:    nil,
-					Provides:    nil,
-					Parameters:  nil,
-					Properties:  nil,
+					Name:       "uiapp",
+					Type:       "html5",
+					Path:       "./",
+					Requires:   nil,
+					Provides:   nil,
+					Parameters: nil,
+					Properties: nil,
 				},
 			},
 			want: []CommandList{
@@ -66,11 +67,45 @@ builders:
 					[]string{"npm install", "grunt", "npm prune --production"},
 				},
 			},
-		}}
+		},
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := mesh(tt.args.modules, commands); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("CommandProvider() = \n got \t\n %v, \n want \t\n %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCommandProvider(t *testing.T) {
+	type args struct {
+		modules models.Modules
+	}
+	tests := []struct {
+		name string
+		args args
+		want []CommandList
+	}{
+		{
+			name: "Command for required module type",
+			args: args{
+				modules: models.Modules{
+					Type: "html5",
+				},
+			},
+			want: []CommandList{
+				{
+					"installing module dependencies & execute grunt & remove dev dependencies",
+					[]string{"npm install", "grunt", "npm prune --production"},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CommandProvider(tt.args.modules); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CommandProvider() = %v, want %v", got, tt.want)
 			}
 		})
 	}
