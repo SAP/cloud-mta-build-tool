7c7a63cd2aa1aba60b9eb92de2e963b4a1ec3f43
tals <shimon.tal@sap.com>
2018-09-15 22:56:21 +0300

    [feat] - module build first phase

--- cmd/commands/build.go
+++ cmd/commands/build.go
@@ -1,7 +1,15 @@
 package commands
 
 import (
+	"path/filepath"
+
 	"github.com/spf13/cobra"
+
+	"cloud-mta-build-tool/cmd/builders"
+	fs "cloud-mta-build-tool/cmd/fsys"
+	"cloud-mta-build-tool/cmd/logs"
+	"cloud-mta-build-tool/cmd/mta/models"
+	"cloud-mta-build-tool/cmd/mta/provider"
 )
 
 // Build module
@@ -10,6 +18,23 @@ var bm = &cobra.Command{
 	Short: "Build module",
 	Long:  "Build module",
 	Run: func(cmd *cobra.Command, args []string) {
-
+		mta, err := provider.MTA(filepath.Join(fs.GetPath(), ""))
+		if err != nil {
+			logs.Logger.Errorf("Not able to parse MTA ", err)
+		}
+		module := buildModule(mta, args[0])
+		logs.Logger.Info(module)
 	},
+}
+
+func buildModule(mta models.MTA, moduleName string) []string {
+	var cmd []string
+	for _, m := range mta.Modules {
+		if m.Name == moduleName {
+			commandProvider := builders.CommandProvider(*m)
+			cmd = commandProvider.Command
+			break
+		}
+	}
+	return cmd
 }
