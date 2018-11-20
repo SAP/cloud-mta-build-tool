9077cdabd41b9876918ce62d08d11b69761bd180
Volkov <alla.volkov@sap.com>
2018-11-20 16:29:08 +0200

    adapt makefile generation to use binaries

--- generator.go
+++ generator.go
@@ -4,3 +4,11 @@ package main
 //go:generate go run ./internal/build-tools/embed.go -source=./configs/commands_cfg.yaml -target=./internal/builders/commands_cfg.go -name=CommandsConfig -package=builders
 //go:generate go run ./internal/build-tools/embed.go -source=./configs/version.yaml -target=./internal/version/version_cfg.go -name=VersionConfig -package=version
 //go:generate go run ./internal/build-tools/embed.go -source=./mta/schema.yaml -target=./mta/mta_schema.go -name=schemaDef -package=mta
+//go:generate go run ./internal/build-tools/embed.go -source=./internal/tpl/base_post_default.txt -target=./internal/tpl/base_post_default.go -name=basePostDefault -package=tpl
+//go:generate go run ./internal/build-tools/embed.go -source=./internal/tpl/base_post_verbose.txt -target=./internal/tpl/base_post_verbose.go -name=basePostVerbose -package=tpl
+//go:generate go run ./internal/build-tools/embed.go -source=./internal/tpl/base_pre_default.txt -target=./internal/tpl/base_pre_default.go -name=basePreDefault -package=tpl
+//go:generate go run ./internal/build-tools/embed.go -source=./internal/tpl/base_pre_verbose.txt -target=./internal/tpl/base_pre_verbose.go -name=basePreVerbose -package=tpl
+//go:generate go run ./internal/build-tools/embed.go -source=./internal/tpl/make_default.txt -target=./internal/tpl/make_default.go -name=makeDefault -package=tpl
+//go:generate go run ./internal/build-tools/embed.go -source=./internal/tpl/make_deployment.txt -target=./internal/tpl/make_deployment.go -name=makeDeployment -package=tpl
+//go:generate go run ./internal/build-tools/embed.go -source=./internal/tpl/make_verbose.txt -target=./internal/tpl/make_verbose.go -name=makeVerbose -package=tpl
+//go:generate go run ./internal/build-tools/embed.go -source=./internal/tpl/make_verbose_dep.txt -target=./internal/tpl/make_verbose_dep.go -name=makeVerboseDep -package=tpl
