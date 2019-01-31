61822534ee7573d396626783e454996244e66c8d
Volkov <alla.volkov@sap.com>
2019-01-31 15:36:29 +0200

    validation removed from assembly

--- cmd/assembly.go
+++ cmd/assembly.go
@@ -44,13 +44,8 @@ var assemblyCommand = &cobra.Command{
 func assembly(source, target, platform string, getWd func() (string, error)) error {
 	logs.Logger.Info("assembling the MTA project...")
 
-	err := artifacts.ExecuteValidation(source, dir.Dep, "semantic", getWd)
-	if err != nil {
-		return errors.Wrap(err, "assembly of the MTA project failed on the MTA descriptor validation")
-	}
-
 	// copy from source to target
-	err = artifacts.CopyMtaContent(source, target, dir.Dep, getWd)
+	err := artifacts.CopyMtaContent(source, target, dir.Dep, getWd)
 	if err != nil {
 		return errors.Wrap(err, "assembly of the MTA project failed when copying the MTA content")
 	}
