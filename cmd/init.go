7cc563140be60eff1618c0723df7a5da310869cc
Volkov <alla.volkov@sap.com>
2019-05-16 17:35:39 +0300

    fix "init" command flags

--- cmd/init.go
+++ cmd/init.go
@@ -6,20 +6,20 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/SAP/cloud-mta-build-tool/internal/tpl"
+	"github.com/SAP/cloud-mta-build-tool/internal/archive"
 )
 
 // flags of init command
 var initCmdSrc string
 var initCmdTrg string
-var initCmdDesc string
 var initCmdMode string
 
 // init flags of init command
 func init() {
 	initCmd.Flags().StringVarP(&initCmdSrc, "source", "s", "", "the path to the MTA project; the current path is default")
 	initCmd.Flags().StringVarP(&initCmdTrg, "target", "t", "", "the path to the generated Makefile folder; the current path is default")
-	initCmd.Flags().StringVarP(&initCmdDesc, "desc", "d", "", "the MTA descriptor; supported values: dev (development descriptor, default value) and dep (deployment descriptor)")
 	initCmd.Flags().StringVarP(&initCmdMode, "mode", "m", "", "the mode of the Makefile generation; supported values: default and verbose")
+	initCmd.Flags().MarkHidden("mode")
 }
 
 var initCmd = &cobra.Command{
@@ -29,7 +29,7 @@ var initCmd = &cobra.Command{
 	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
 		// Generate build script
-		err := tpl.ExecuteMake(initCmdSrc, initCmdTrg, initCmdDesc, initCmdMode, os.Getwd)
+		err := tpl.ExecuteMake(initCmdSrc, initCmdTrg, dir.Dev, initCmdMode, os.Getwd)
 		logError(err)
 	},
 }
