fd7fbd94acb0097c211225181869ed1614c698cc
tal-sapan <tal.sapan@sap.com>
2020-01-19 17:46:43 +0200

    Revert "rel 1.0.72"

--- integration/testdata/node/Gruntfile.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (grunt) {
-	"use strict";
-	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
-	grunt.registerTask("default", [
-		"clean",
-		"lint",
-		"build"
-	]);
-};
