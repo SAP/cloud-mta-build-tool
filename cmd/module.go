bbdd294074de9803ea9fa6858c356ca094413d5b
i019379 <alla.volkov@sap.com>
2020-02-11 15:32:27 +0200

    fix command

--- cmd/module.go
+++ cmd/module.go
@@ -63,6 +63,7 @@ func init() {
 		"The MTA extension descriptors")
 	soloBuildModuleCmd.Flags().StringVarP(&soloBuildModuleCmdModule, "module", "m", "",
 		"The name of the module")
+	soloBuildModuleCmd.MarkFlagRequired("module")
 }
 
 // soloBuildModuleCmd - Build module command used stand alone
@@ -78,7 +79,7 @@ var soloBuildModuleCmd = &cobra.Command{
 	},
 	Hidden:        false,
 	SilenceUsage:  false,
-	SilenceErrors: false,
+	SilenceErrors: true,
 }
 
 // buildModuleCmd - Build module command that is used in Makefile
