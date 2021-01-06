f4e9542a9eae4c1c59b279037e28de409529335c
i019379 <alla.volkov@sap.com>
2021-01-06 16:53:10 +0200

    fix message

--- internal/commands/deprecated_builders.go
+++ internal/commands/deprecated_builders.go
@@ -3,7 +3,7 @@ package commands
 import "github.com/SAP/cloud-mta-build-tool/internal/logs"
 
 // List of deprecated builders
-var deprecatedBuilders = map[string]string{"maven_deprecated": `the "maven_deprecated" builder is deprecated and will be removed on July, 2021`}
+var deprecatedBuilders = map[string]string{"maven_deprecated": `the "maven_deprecated" builder is deprecated and will be removed on July 2021`}
 
 func checkDeprecatedBuilder(builder string) {
 	warn := deprecatedBuilders[builder]
